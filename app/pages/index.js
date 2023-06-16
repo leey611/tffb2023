ce84300409f2a8dbf03af84ab1df312aba16f913
leey611 <leey611@newschool.edu>
2023-06-16 00:37:45 -0400

    first commit

---
+++ app/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a basic Next.js page.</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
