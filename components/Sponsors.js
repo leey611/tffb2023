7968fb5f16e7d59151cbadaac1bb8ef17983334a
leey611 <leey611@newschool.edu>
2023-07-19 13:53:38 -0400

    for test deploy 2

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,17 +1,7 @@
-let airtableApiKey
-let airtableBaseId
-let airtableTableId
-let airtableTableSponsorsViewId
-
-if (process.env.NODE_ENV !== 'production') {
-  airtableApiKey = process.env.AIRTABLE_API_KEY
-  airtableBaseId = process.env.AIRTABLE_BASE_ID
-  airtableTableId = process.env.AIRTABLE_TABLE_SPONSORS_ID
-  airtableTableSponsorsViewId = process.env.AIRTABLE_TABLE_SPONSORS_VIEW_ID
-} else {
-  //airtableApiKey = process.env.CLIENT_KEY;
-  //airtableBaseId = process.env.CLIENT_SECRET;
-}
+let airtableApiKey = process.env.AIRTABLE_API_KEY
+let airtableBaseId = process.env.AIRTABLE_BASE_ID
+let airtableTableId = process.env.AIRTABLE_TABLE_SPONSORS_ID
+let airtableTableSponsorsViewId = process.env.AIRTABLE_TABLE_SPONSORS_VIEW_ID
 
 
 async function getSponsors() {
