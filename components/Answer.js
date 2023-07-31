4d06ccb1e348b76c71b2fdded2c9709ff2de5a0f
leey611 <leey611@newschool.edu>
2023-07-31 15:41:02 -0400

    resolve chinese italic not working bug by changing package from react-markdown to remarkable

--- components/Answer.js
+++ components/Answer.js
@@ -1,11 +1,18 @@
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import { Remarkable } from 'remarkable';
+const md = new Remarkable();
+
+function renderMarkdownToHTML(markdown) {
+    // This is ONLY safe because the output HTML
+    // is shown to the same user, and because you
+    // trust this Markdown parser to not have bugs.
+    const renderedHTML = md.render(markdown);
+    return {__html: renderedHTML};
+  }
 export default function Answer({ answer }) {
+    const markup = renderMarkdownToHTML(answer);
     return (
         <div className="answer text-primary">
-            <div>
-                <ReactMarkdown children={answer} remarkPlugins={[remarkGfm]} />
-            </div>
+            <div dangerouslySetInnerHTML={markup}></div>
         </div>
     )
 }
