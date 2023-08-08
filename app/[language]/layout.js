d19668b4f8d486e67bc9a8eebe30b82022a19226
yu <43624848+leey611@users.noreply.github.com>
2023-08-08 16:48:00 -0400

    remove html body except root layout

--- app/[language]/layout.js
+++ app/[language]/layout.js
@@ -1,30 +1,30 @@
 import '../globals.css'
 import { validateLanguage, sectionTitles } from '../../utils/helpers'
 
-export async function generateMetadata({ params }) {
-  const languageRoute = validateLanguage(params.language) ? params.language : 'en'
-  const siteText = sectionTitles[languageRoute]
-  const title = `${siteText.donate} | ${siteText.siteTitle}`
-  const description = sectionTitles[languageRoute].description
-  return {
-    title,
-    description,
-  }
-}
+// export async function generateMetadata({ params }) {
+//   const languageRoute = validateLanguage(params.language) ? params.language : 'en'
+//   const siteText = sectionTitles[languageRoute]
+//   const title = `${siteText.donate} | ${siteText.siteTitle}`
+//   const description = sectionTitles[languageRoute].description
+//   return {
+//     title,
+//     description,
+//   }
+// }
 
 export default function RootLayout({ children, params }) {
     return (
       // <Scaffolding lang={params.language}>
-      //   { children }
+        { children }
       // </Scaffolding>
         // <html lang={params.language}>
         //     <body>
         //         <div>{params.language}</div>
         //     </body>
         // </html>
-        <html>
-          <body>{children}</body>
-        </html>
+        // <html>
+        //   <body>{children}</body>
+        // </html>
       
     );
   }
