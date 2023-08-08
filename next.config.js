2baa9c17fc5dd599ead481e286f090b7a70c3e17
yu <43624848+leey611@users.noreply.github.com>
2023-08-08 16:56:03 -0400

    remove i18n in next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const path = require('path')
  
 module.exports = {
-  i18n: {
-    locales: ['en','de', 'zh-TW'],
-    defaultLocale: 'en',
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
