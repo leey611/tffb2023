b3d68c65f05d148f3439afb903d7cc349e6a4b34
yu <43624848+leey611@users.noreply.github.com>
2023-08-08 23:31:53 -0400

    UPDATE: marquee logo to no text logo

--- components/Marquee.js
+++ components/Marquee.js
@@ -7,15 +7,15 @@ export default function Marquee(props) {
         <Link href={link}>
             <div className="marquee text-h4">
                 <span>
-                    <img className="inline-block w-10" src="../img/logo.png"></img>
+                    <img className="inline-block w-7 px-1" src="../img/logo_no_text.png"></img>
                     {content}
-                    <img className="inline-block w-10" src="../img/logo.png"></img>
+                    <img className="inline-block w-7 px-1" src="../img/logo_no_text.png"></img>
                     {content}
-                    <img className="inline-block w-10" src="../img/logo.png"></img>
+                    <img className="inline-block w-7 px-1" src="../img/logo_no_text.png"></img>
                     {content}
-                    <img className="inline-block w-10" src="../img/logo.png"></img>
+                    <img className="inline-block w-7 px-1" src="../img/logo_no_text.png"></img>
                     {content}
-                    <img className="inline-block w-10" src="../img/logo.png"></img>
+                    <img className="inline-block w-7 px-1" src="../img/logo_no_text.png"></img>
                 </span>
             </div>
         </Link>
