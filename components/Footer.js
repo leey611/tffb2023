47dc58593d11e71c9884f46fc247a34c53c30eff
yu <43624848+leey611@users.noreply.github.com>
2023-08-16 12:56:55 -0400

    add calendar icon and link to social

--- components/Footer.js
+++ components/Footer.js
@@ -13,6 +13,8 @@ export default function Footer({ language }) {
             <SocialHandle logo="../img/social_fb.svg" link="https://www.facebook.com/ImpressionTaiwan/" />
             <SocialHandle logo="../img/social_ig.svg" link="https://www.instagram.com/impressiontaiwan/" />
             <SocialHandle logo="../img/social_yt.svg" link="https://www.youtube.com/@taiwanfilmfestivalberlinby1737" />
+            <SocialHandle logo="../img/social_calendar.svg" link="https://calendar.google.com/calendar/u/0/embed?src=a155c1623d738c727a5e1a35da5940f9b9f5393594e27056ec9a865e06bdf0b2@group.calendar.google.com&ctz=Asia/Taipei" />
+            
           </div>
           <div className="z-50">
             <Link href={`/${language}/about`}><button className="border-2 border-secondary py-3 px-5 rounded-full text-h4 font-special font-medium">{sectionTitles[language].aboutUs}</button>
