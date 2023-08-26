6d739371e9d5731047d93dfe49e5bc9171816631
yu <43624848+leey611@users.noreply.github.com>
2023-08-26 11:44:41 -0400

    about this year h2 center on mobile

--- components/HomeView.js
+++ components/HomeView.js
@@ -148,7 +148,7 @@ export default async function HomeView({ language }) {
                 <SpecialTitle year={websiteGlobalFields['Year']} title={aboutSectionTitle} img="../img/hero2Img.png" />
                 <div>
                     {aboutThisYear?.map(obj => <div key={obj.id} className="my-4">
-                        <h2 className="text-left md:text-center font-special text-h2 font-semibold mb-2">{obj.fields[`Question_${language}`]}</h2>
+                        <h2 className="text-center font-special text-h2 font-semibold mb-2">{obj.fields[`Question_${language}`]}</h2>
                         <div>
                             <RichText content={obj.fields[`Answer_${language}`]}/>
                         </div>
